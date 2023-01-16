20c7eb738223b7cd9b93d5b3c89c7b746cd44329
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2023-01-16 23:08:50 +0100

    add js file

---
+++ js/game.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', init, false);
+var currentColor = "R"
+
+function init(){
+    console.log("---page loaded---");
+    let board = document.getElementById("board");
+    board.addEventListener("click", color);
+}
+
+
+function initGrid(){  
+    var parent = document.getElementById('div');
+    parent.insertAfter(link, parent.firstChild);
+
+    let board = document.getElementById("board");
+    for(let i = 0 ; i < 6 ; i++){
+        for(let k = 0 ; k < 7 ; k++){
+            var link = document.createElement('a');
+            link.setAttribute('href', 'mypage.htm');
+        }
+    }
+}
+
+
+function color(){
+    let element =  event.target;
+    if(element.classList.contains("case")){
+        if(currentColor == "R"){
+            element.style.background = "red";
+            currentColor = "Y"
+        }
+        else{
+            element.style.background = "yellow";
+            currentColor = "R"
+        }  
+    }
+}
